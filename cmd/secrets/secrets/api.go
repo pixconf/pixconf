2b15facebca55fb75102c3c963007f41db956979
Vitaliy Vasylenko <me@vitalvas.com>
2023-11-17 10:52:53 -0500

    feat: enforce tls config

--- cmd/secrets/secrets/api.go
+++ cmd/secrets/secrets/api.go
@@ -59,11 +59,25 @@ func (s *Secrets) ListenAndServe() error {
 		return err
 	}
 
+	// https://ssl-config.mozilla.org/
+	tlsCfg := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		},
+	}
+
 	s.srv = &http.Server{
 		Addr:              s.config.APIAddress,
 		Handler:           router,
-		ReadTimeout:       10 * time.Second,
-		ReadHeaderTimeout: 10 * time.Second,
+		TLSConfig:         tlsCfg,
+		ReadTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
 		IdleTimeout:       60 * time.Second,
 	}
 
--- internal/autocert/tls_config.go
+++ internal/autocert/tls_config.go
@@ -8,8 +8,20 @@ func GetTLSConfig(cert, privateKey []byte) (*tls.Config, error) {
 		return nil, err
 	}
 
+	// https://ssl-config.mozilla.org/
 	config := &tls.Config{
-		Certificates: []tls.Certificate{certificate},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		},
+		Certificates: []tls.Certificate{
+			certificate,
+		},
 	}
 
 	return config, nil
