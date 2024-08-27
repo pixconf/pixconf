71de09617a051cf5cd6cbe585070802f252898f8
Vitaliy Vasylenko <me@vitalvas.com>
2024-08-26 22:01:19 -0400

    feat: small auth refactor

--- cmd/server/agent/mqttauth/hook_test.go
+++ cmd/server/agent/mqttauth/hook_test.go
@@ -1,11 +1,9 @@
 package mqttauth
 
 import (
-	"log/slog"
 	"testing"
 
 	mqtt "github.com/mochi-mqtt/server/v2"
-	"github.com/mochi-mqtt/server/v2/packets"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,13 +18,4 @@ func TestHookProvides(t *testing.T) {
 	require.True(t, h.Provides(mqtt.OnACLCheck))
 	require.True(t, h.Provides(mqtt.OnConnectAuthenticate))
 	require.False(t, h.Provides(mqtt.OnPublished))
-}
-
-func TestHookOnConnectAuthenticate(t *testing.T) {
-	h := NewHook(slog.New(slog.NewTextHandler(nil, nil)))
-
-	client := new(mqtt.Client)
-	client.Net.Remote = "192.0.2.11"
-
-	require.True(t, h.OnConnectAuthenticate(client, packets.Packet{}))
 }
