ac03025926aae6643a45969e42e2f0cfd21a76ab
Vitaliy Vasylenko <me@vitalvas.com>
2024-09-16 23:00:50 -0400

    feat: add expire

--- pkg/xkit/mqtt.go
+++ pkg/xkit/mqtt.go
@@ -2,6 +2,7 @@ package xkit
 
 import (
 	"fmt"
+	"time"
 
 	mqtt "github.com/mochi-mqtt/server/v2"
 	"github.com/mochi-mqtt/server/v2/packets"
@@ -12,6 +13,7 @@ type MQTTPublishRequest struct {
 	Topic      string
 	Payload    []byte
 	Properties packets.Properties
+	Expiry     time.Duration
 }
 
 // hack: https://github.com/mochi-mqtt/server/issues/428
@@ -19,7 +21,7 @@ func MQTTPublish(server *mqtt.Server, request *MQTTPublishRequest) error {
 	clientID := fmt.Sprintf("inline-%s", xid.New().String())
 	client := server.NewClient(nil, "local", clientID, true)
 
-	return server.InjectPacket(client, packets.Packet{
+	packet := packets.Packet{
 		FixedHeader: packets.FixedHeader{
 			Type: packets.Publish,
 		},
@@ -27,5 +29,12 @@ func MQTTPublish(server *mqtt.Server, request *MQTTPublishRequest) error {
 		Payload:    request.Payload,
 		PacketID:   0,
 		Properties: request.Properties,
-	})
+		Created:    time.Now().Unix(),
+	}
+
+	if request.Expiry > 0 {
+		packet.Expiry = time.Now().Add(request.Expiry).Unix()
+	}
+
+	return server.InjectPacket(client, packet)
 }
